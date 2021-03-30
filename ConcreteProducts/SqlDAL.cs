baf77a8d433f54895f60942d8f15f2359e785b6c
Helena Copppieters <hcoppieters@hotmail.com>
2021-03-30 13:40:27 +0200

    Oplossing oefening Factory Method design pattern

---
+++ ConcreteProducts/SqlDAL.cs
@@ -0,0 +1,32 @@
+﻿using Product;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ConcreteProducts
+{
+    public class SqlDAL : DALBaseClass
+    {
+        public SqlDAL() { }
+        public SqlDAL(string connectionString) {
+            this.ConnectionString = "Data Source=.;Initial Catalog=master;Integrated Security=true";
+        }
+        public override IDbCommand GeDataProviderCommand()
+        {
+            return new SqlCommand();
+        }
+
+        public override IDbConnection GetDataProviderConnection()
+        {
+            return new SqlConnection();
+        }
+
+        public override IDbDataAdapter GetDataProviderDataAdapter()
+        {
+            return new SqlDataAdapter();
+        }
+    }
+}
